d3b16d309287e2d714e61e50bd1145eb5ba141e9
Anil <bridgpal@gmail.com>
2013-01-07 13:45:34 -0500

    twilio

--- web.rb
+++ web.rb
@@ -1,5 +1,36 @@
 require 'sinatra'
+require 'twilio-ruby'
+
+account_sid = ENV['TWILIO_ACCT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+def concert_find(band)
+url = "http://xpn.org/concerts-events/concert-calendar"
+data = Nokogiri::HTML(open(url))
+string = "Sorry, but we could not find a show"
+#parse concert data
+concerts = data.css("tbody").css("tr")
+concerts.each do |concerts|
+	if concerts.css(".cell1").text.downcase.include? band.downcase
+ 	string = concerts.at_css(".cell1").text + ' | ' + concerts.at_css(".cell0").text + ' @ ' +concerts.at_css(".cell2").text 
+	
+	break
+	end
+	
+end
+string
+end
 
 get '/' do
-  "Hello, world"
+  "philly concerts by sms test"
+
+
+post '/sms' do
+  concert = concert_find(params[:Body])
+  twiml = Twilio::TwiML::Response.new do |r|
+  	r.Sms concert
+  end
+  twiml.text
+end
+
 end
